16e34d3cb44f3ccafbd6a21d30e90496546abe8d
briansterling93 <41628472+briansterling93@users.noreply.github.com>
2019-08-17 11:49:09 -0600

    styling changes

--- src/comps/MarkDown.js
+++ src/comps/MarkDown.js
@@ -2,43 +2,35 @@ import React, { Component } from 'react';
 import ReactMarkdown from 'react-markdown';
 import Styling from './Styling.scss';
 
-class MarkDown extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      header:  "# This is a header",
-      subHeader: '## This is a subheader'
-    }
-  }
+class MarkDown extends Component {
+  // constructor(props) {
+  //   super(props);
+  //   this.state = {
+  //     header:  "# This is a header",
+  //     subHeader: '## This is a subheader'
+  //   }
+  // }
     // updateText(e) {   
     //     const output = document.getElementById('userText').innerHTML = e.target.value;
     // }
     render() {
-      // const placeholder = 
-      // '# This is a header'
+        const placeholder =
+          '# Welcome to my Markdown Previewer!<hr />\n\n## This is a subheading<hr />\n\n### And here are a few other cool gadgets:\n\nHere is a cool [Link](https://blank.org/) !\n\nYou can make text **bold**, _italic_, better yet **_both_**!..\n\n1.Create\n\n2.Numbered\n\n3.Lists!\n\n-Even Create..\n\n*Asterick\n\n-Lists! :D\n\n*Last but **NOT** least ;) - create cool images like the one below:\n\n![React Logo w/ Text](https://goo.gl/Umyytc)'
 
-    
+
         return (
           <div id='element'>
               <div id='editor'>
                 <header><h2 className='headers'>Editor</h2></header>
-                <textarea rows="14" cols="88" className='editorColor' value={placeholder} onChange={this.updateText}>
-                
+                <textarea rows="14" cols="88" className='editorColor' onChange={this.updateText}>
+                  {placeholder}
                 </textarea>
               </div>
                   <br />
                     <div id='preview'>
                       <header><h2 className='headers'>Previewer</h2></header>
                         <div id='userText'>
-                          <div id='headerTxt'><ReactMarkdown source={this.state.header} /></div>
-                            <div id='subHeader'><ReactMarkdown source={this.state.subHeader} /></div>
-                              <div id='link'></div>
-                                <div id='inlineCode'></div>
-                                  <div id='codeBlock'></div>
-                                    <div id='listItem'></div>
-                                      <div id='blockQoute'></div>
-                                        <div id='image'></div>
-                                          <div id='boldedText'></div>
+                        <ReactMarkdown source={placeholder} escapeHtml={false} />
                         </div>
                      </div>
           </div>
