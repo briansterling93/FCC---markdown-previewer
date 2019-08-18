9a41bc6075daf4b710c084af45753d397d13fe3d
briansterling93 <41628472+briansterling93@users.noreply.github.com>
2019-08-18 02:25:58 -0600

    styling changes

--- src/comps/MarkDown.js
+++ src/comps/MarkDown.js
@@ -6,9 +6,23 @@ class MarkDown extends Component {
   updateText(e) {
     document.getElementById("userText").textContent = e.target.value;
   }
+
+
   render() {
     const placeholder =
-      "# Welcome to my Markdown Previewer!<hr />\n\n## This is a subheading<hr />\n\n### And here are a few other cool gadgets:\n\nHere is a cool [Link](https://blank.org/) !\n\nYou can make text **bold**, _italic_, better yet **_both_**!..\n\n1.Create\n\n2.Numbered\n\n3.Lists!\n\n-Even Create..\n\n*Asterisk\n\n-Lists! :D\n\n*Last but **NOT** least ;) - create cool images like the one below:\n\n![React Logo w/ Text](https://goo.gl/Umyytc)";
+      `# Welcome to my Markdown Previewer!<hr />
+      \n\n## This is a subheading<hr />
+      \n\n### And here are a few other cool gadgets:
+      \n\nHere is a cool [Link](https://blank.org/) !
+      \n\nYou can make text **bold**, _italic_, better yet **_both_**!..
+      \n\n1.Create
+      \n\n2.Numbered
+      \n\n3.Lists!
+      \n\n-Even Create..
+      \n\n*Asterisk
+      \n\n-Lists! :D
+      \n\n*Last but **NOT** least ;) - create cool images like the one below:
+      \n\n![React Logo w/ Text](https://goo.gl/Umyytc)`;
 
     return (
       <div id="element">
@@ -20,7 +34,6 @@ class MarkDown extends Component {
             rows="14"
             cols="88"
             className="editorColor"
-            onChange={this.updateText}
           >
             {placeholder}
           </textarea>
